5c3d184097ffb4bff826adf8e38a47263c726079
kipkoechke <chepkwonyke1@gmail.com>
2023-02-19 09:31:03 +0300

    Passing props

---
+++ React Basics/passingprops/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Bellon" />
+            <Heading firstName="Kelvin" />
+        </div>
+    );
+};
+
+export default App;
